c04ba5b8423f970e08a5b2afd5c4d30edee693e6
GongoraLeo <gongora.leo@gmail.com>
2025-03-31 15:46:45 +0200

    finalizadas migraciones. controladores y modelos 1

---
+++ app/Http/Controllers/AutoresController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Autores;
+use Illuminate\Http\Request;
+
+class AutoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Autores $autores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Autores $autores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Autores $autores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Autores $autores)
+    {
+        //
+    }
+}
